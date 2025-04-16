6eddce315e637c65c13a48305844ccc55e164d03
williamfisher9 <fisher.a.william@gmail.com>
2025-04-16 03:31:30 -0500

    enhanced btn look and feel

--- src/constants/Constants.jsx
+++ src/constants/Constants.jsx
@@ -1,2 +1,2 @@
-//export const BACKEND_URL = "https://willtechbooth.dev/biteandsip"
-export const BACKEND_URL = "http://localhost:8080"
+export const BACKEND_URL = "https://willtechbooth.dev/biteandsip"
+//export const BACKEND_URL = "http://localhost:8080"
